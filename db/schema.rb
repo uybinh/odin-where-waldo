482259e972b3078a719209ccc282744216dc8203
Uy Binh <uybinh@gmail.com>
2018-09-19 10:53:14 +0700

    Add player model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_19_032850) do
+ActiveRecord::Schema.define(version: 2018_09_19_035149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,4 +21,14 @@ ActiveRecord::Schema.define(version: 2018_09_19_032850) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "players", force: :cascade do |t|
+    t.string "name"
+    t.integer "score"
+    t.bigint "level_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["level_id"], name: "index_players_on_level_id"
+  end
+
+  add_foreign_key "players", "levels"
 end
