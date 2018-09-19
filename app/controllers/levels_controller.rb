3ff27cd5d55a4ca5ba3ee56d2b8c7b323746992c
Uy Binh <uybinh@gmail.com>
2018-09-19 15:20:22 +0700

    Add levels controller

---
+++ app/controllers/levels_controller.rb
@@ -0,0 +1,6 @@
+class LevelsController < ApplicationController
+
+  def show
+    @level = Level.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'singlepages#index'
+  resources :levels, only: :show
 end
