7ef8bf2dad2293a5a8f74511338f9094f9b84dfc
Uy Binh <uybinh@gmail.com>
2018-09-20 11:19:48 +0700

    Add key_cord property to level model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_19_035149) do
+ActiveRecord::Schema.define(version: 2018_09_20_040753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2018_09_19_035149) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "key_cord"
   end
 
   create_table "players", force: :cascade do |t|
