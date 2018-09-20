89fd7f4692df649cae9cf85f9068af22071e4fc3
Uy Binh <uybinh@gmail.com>
2018-09-20 11:18:46 +0700

    Add respond to json format on level url

--- app/controllers/levels_controller.rb
+++ app/controllers/levels_controller.rb
@@ -2,5 +2,9 @@ class LevelsController < ApplicationController
 
   def show
     @level = Level.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json { render json: @level.key_cord }
+    end
   end
 end
