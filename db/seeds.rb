4a1e53e2422a75239d1ae6683977e0481eb385c4
Uy Binh <uybinh@gmail.com>
2018-09-20 15:48:35 +0700

    Edit seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
-Level.create(name: 'easy')
-Level.create(name: 'medium')
-Level.create(name: 'hard')
+Level.create(name: 'easy', key_cord: '{"from":{"x":310,"y":245},"to":{"x":350,"y":305}}')
+Level.create(name: 'medium', key_cord: '{"from":{"x":993,"y":538},"to":{"x":1033,"y":598}}')
+Level.create(name: 'hard', key_cord: '{"from":{"x":705,"y":236},"to":{"x":745,"y":296}}')
