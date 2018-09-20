91bce3f891157f976652060ea24ba1de60947c4c
Uy Binh <uybinh@gmail.com>
2018-09-20 14:59:30 +0700

    Add function to check selected position

--- app/assets/javascripts/levels.js
+++ app/assets/javascripts/levels.js
@@ -2,13 +2,16 @@ document.addEventListener("turbolinks:load", () => {
   app()
 })
 
-function app() {
+async function app() {
   const canvas = document.querySelector("#canvas")
+
   if (canvas) {
+    const key = await fetchKey().then(saveKeyToStorage)
     canvas.addEventListener("click", event => {
       const pos = getPosition(canvas, event)
       selectTarget(pos)
       console.log(pos)
+      checkPosition(pos, key)
     })
   }
 }
@@ -32,4 +35,25 @@ function newTarget() {
     document.querySelector("#target") || document.createElement("div")
   target.id = "target"
   return target
+}
+
+function fetchKey() {
+  url = window.location.href + ".json"
+  return fetch(url).then(res => res.json())
+}
+
+const saveKeyToStorage = key => {
+  window.localStorage.setItem("key", JSON.stringify(key))
+  return key
+}
+
+const checkPosition = (pos, key) => {
+  if (
+    pos.x > key.from.x &&
+    pos.x < key.to.x &&
+    pos.y > key.from.y &&
+    pos.y < key.to.y
+  ) {
+    alert("greate")
+  }
 }
